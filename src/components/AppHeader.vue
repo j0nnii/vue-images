1d0c9eba2dd34290adff7448f553176abd6bb61a
Jonni Tammisto <jonni.tammisto@skhole.fi>
2018-08-22 08:18:13 +0300

    Init

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="ui secondary pointing menu">
+    <a href="/" class="item active">
+      Image Storage
+    </a>
+    <div class="right menu">
+      <a class="ui item">
+        Logout
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+};
+</script>
