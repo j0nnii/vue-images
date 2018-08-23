526bd7d7189c88714da2c8b67a4e9c254f63ed00
Jonni Tammisto <jonni.tammisto@skhole.fi>
2018-08-23 15:59:29 +0300

    Persisting local state

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,18 +5,23 @@
     </a>
 
     <div class="right menu">
-      <a class="ui item" href="#" @click="login">
+      <a class="ui item" href="#" v-if="isLoggedIn">
+        Logout
+      </a>
+      <a class="ui item" href="#" @click="login" v-else>
         Login
       </a>
+
     </div>
   </div>
 </template>
 
 <script>
-import {mapActions} from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'AppHeader',
+  computed: mapGetters(['isLoggedIn']),
   methods: mapActions(['login'])
 };
 </script>
